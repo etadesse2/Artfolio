0c83b607f7ee113ba3c3ef2ddccb765aba8535d1
sarahyosef <sarahyosefux@gmail.com>
2024-04-23 15:36:35 -0400

    ui added for login but background image needed

--- artfolio/lib/login.dart
+++ artfolio/lib/login.dart
@@ -1,4 +1,5 @@
 import 'package:artfolio/main_screen.dart';
+import 'package:artfolio/registration.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:fluttertoast/fluttertoast.dart';
@@ -40,40 +41,132 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Login")),
-      body: Container(
-        padding: EdgeInsets.all(20.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-              TextFormField(
-                onChanged: (value) => email = value,
-                decoration: InputDecoration(labelText: 'Email'),
-                validator: (value) {
-                  if (value!.isEmpty || !value.contains('@')) {
-                    return 'Please enter a valid email';
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                onChanged: (value) => password = value,
-                obscureText: true,
-                decoration: InputDecoration(labelText: 'Password'),
-                validator: (value) {
-                  if (value!.isEmpty || value.length < 6) {
-                    return 'Password must be at least 6 characters';
-                  }
-                  return null;
-                },
-              ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: signInUser,
-                child: Text('Login'),
-              ),
-            ],
+      backgroundColor: Colors.purple,
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(40.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                const Padding(
+                  padding: EdgeInsets.only(top: 135.0),
+                  child: Text(
+                    'ART\nFOLIO.',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 70,
+                      fontWeight: FontWeight.w900,
+                      letterSpacing: 3,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Container(
+                  width: 300,
+                  decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                          color: const Color.fromARGB(255, 123, 123, 123)
+                              .withOpacity(0.5),
+                          spreadRadius: 5,
+                          blurRadius: 7,
+                          offset: const Offset(0, 7))
+                    ],
+                  ),
+                  child: TextFormField(
+                    onChanged: (value) => email = value,
+                    obscureText: true,
+                    validator: (value) {
+                      if (value!.isEmpty || !value.contains('@')) {
+                        return 'Please enter a valid email';
+                      }
+                      return null;
+                    },
+                    decoration: InputDecoration(
+                        hintText: 'EMAIL',
+                        fillColor: Colors.white,
+                        hintStyle: const TextStyle(color: Color(0xFFDDDDDD)),
+                        filled: true,
+                        contentPadding: const EdgeInsets.symmetric(
+                            vertical: 27, horizontal: 15),
+                        border: OutlineInputBorder(
+                            borderSide: BorderSide.none,
+                            borderRadius: BorderRadius.circular(20))),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Container(
+                  width: 300,
+                  decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 5,
+                          blurRadius: 7,
+                          offset: const Offset(0, 7))
+                    ],
+                  ),
+                  child: TextFormField(
+                    onChanged: (value) => password = value,
+                    obscureText: true,
+                    validator: (value) {
+                      if (value!.isEmpty || value.length < 6) {
+                        return 'Password must be at least 6 characters';
+                      }
+                      return null;
+                    },
+                    decoration: InputDecoration(
+                        hintText: 'PASSWORD',
+                        fillColor: Colors.white,
+                        hintStyle: const TextStyle(color: Color(0xFFDDDDDD)),
+                        filled: true,
+                        contentPadding: const EdgeInsets.symmetric(
+                            vertical: 27, horizontal: 15),
+                        border: OutlineInputBorder(
+                            borderSide: BorderSide.none,
+                            borderRadius: BorderRadius.circular(20))),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                    style: ButtonStyle(
+                        shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20))),
+                        padding: MaterialStateProperty.all(
+                            const EdgeInsets.symmetric(
+                                horizontal: 110, vertical: 30)),
+                        backgroundColor:
+                            MaterialStateProperty.all(Colors.black)),
+                    onPressed: signInUser,
+                    child: const Text(
+                      "LOGIN",
+                      style: TextStyle(color: Colors.white, fontSize: 15),
+                    )),
+                Center(
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 150.0),
+                    child: GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => RegistrationScreen()));
+                      },
+                      child: const Text(
+                        "REGISTER",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 15,
+                            fontWeight: FontWeight.w600),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
