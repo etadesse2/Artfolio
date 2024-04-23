50deb433512fb4c14436e4a35fd0a3007b2c8f38
sarahyosef <sarahyosefux@gmail.com>
2024-04-23 15:36:11 -0400

    initial navigation to login instead of registration

--- artfolio/lib/main.dart
+++ artfolio/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:artfolio/login.dart';
 import 'package:artfolio/registration.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -21,7 +22,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: RegistrationScreen(),
+      home: LoginScreen(),
     );
   }
 }
