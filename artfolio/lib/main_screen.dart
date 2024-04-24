ce47392ab8818c183dd33153f53266e6cc0da4ba
Euale Tadesse <81382511+etadesse2@users.noreply.github.com>
2024-04-24 10:32:05 -0400

    edited the appbar and bottoom navigation bar

--- artfolio/lib/main_screen.dart
+++ artfolio/lib/main_screen.dart
@@ -23,18 +23,30 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Artfolio')),
+      appBar: AppBar(
+        title: Image.asset(
+          'assets/images/header.png',
+          fit: BoxFit
+              .cover, // Ensures the image covers the AppBar's title area well
+          height:
+              kToolbarHeight, // Sets the image height to the height of the AppBar
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+      ),
       body: Center(
         child: _widgetOptions.elementAt(_selectedIndex),
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: <BottomNavigationBarItem>[
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Feed'),
           BottomNavigationBarItem(
-              icon: Icon(Icons.business), label: 'Portfolio'),
+              icon: Icon(Icons.art_track_rounded), label: 'Feed'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.design_services_outlined), label: 'Portfolio'),
         ],
         currentIndex: _selectedIndex,
-        selectedItemColor: Colors.amber[800],
+        selectedItemColor:
+            Colors.black, // Ensures the selected item is visibly highlighted
         onTap: _onItemTapped,
       ),
     );
