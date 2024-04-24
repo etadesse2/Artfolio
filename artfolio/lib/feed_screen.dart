97a984689a67631522fccc8cdab492928d88002e
Euale Tadesse <81382511+etadesse2@users.noreply.github.com>
2024-04-24 14:44:32 -0400

    art catagory filtering buttons added

--- artfolio/lib/feed_screen.dart
+++ artfolio/lib/feed_screen.dart
@@ -1,19 +1,58 @@
 import 'package:flutter/material.dart';
 
-class FeedScreen extends StatelessWidget {
-  final bool hasFeed; // This flag will simulate whether there's feed content
+class FeedScreen extends StatefulWidget {
+  @override
+  _FeedScreenState createState() => _FeedScreenState();
+}
 
-  // Constructor to accept feed status, defaulting to false
-  FeedScreen({this.hasFeed = false});
+class _FeedScreenState extends State<FeedScreen> {
+  String selectedCategory = 'All'; // Default category
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Feed"),
+        leading:
+            _buildFilterChipBar(), // Place the filter chip bar on the left side
+        leadingWidth: MediaQuery.of(context)
+            .size
+            .width, // Extend the width of the leading area to occupy full AppBar width
       ),
       body: Center(
-        child: hasFeed ? Text("Here's your feed") : Text("No feed yet"),
+        child: Text("Displaying content for: $selectedCategory"),
+      ),
+    );
+  }
+
+  Widget _buildFilterChipBar() {
+    List<String> categories = [
+      'All',
+      'Painting',
+      'Sketch',
+      'Photography',
+      'Ceramics',
+      'Others'
+    ];
+
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      padding: EdgeInsets.symmetric(vertical: 10),
+      child: Row(
+        children: categories
+            .map((category) => Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8),
+                  child: ChoiceChip(
+                    label: Text(category),
+                    selected: selectedCategory == category,
+                    onSelected: (bool selected) {
+                      setState(() {
+                        if (selected) selectedCategory = category;
+                        // Here you would also trigger a fetch or filter of the feed data based on the selected category
+                      });
+                    },
+                  ),
+                ))
+            .toList(),
       ),
     );
   }
