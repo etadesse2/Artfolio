7176dc2269af4e61183725abf7f36e9a0d745e79
sarahyosef <sarahyosefux@gmail.com>
2024-04-24 22:41:47 -0400

    rm unneccesary lines

--- artfolio/lib/portfolio_page.dart
+++ artfolio/lib/portfolio_page.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-
 void main() {
   runApp(MaterialApp(
     home: PortfolioScreen(),
